504b50e31433c59ab128c0bd85a8a066925c80f2
manuelmenama <manuel.mena090589@gmail.com>
2022-10-21 19:17:21 +0200

    second attempt

--- primo_attempt/js/script.js
+++ primo_attempt/js/script.js
@@ -6,10 +6,31 @@ console.log('Loaded "Campo Minato"');
 //4 creare bottone di start
 //5 creare un una select con tre livelli di difficoltà (1 - 100(10), 2 - 81(9), 3 - 49(7))
 
+//SECONDA PARTE
+/**
+ * -creo variabile con lo score di gioco
+ * -creo array di 16 numeri bomba
+ * -creo variabile del numero massimo di bombe generate
+ * 
+ * GENERARE LE BOMBE
+ * -creo 16 numeri generati casualmente da 1 a grandezza playground
+ * 
+ * CONDIZIONI DI VINCITA
+ * -se casella premuta è uguale ad una bomba, fine gioco, restituisco il punteggio
+ * -se clicco tutte le caselle senza bomba, fine gioco
+ * FINE GIOCO
+ * -quando calpesto una bomba tutte le bombe si accendono
+ * -il playground diventa incliccabile
+ * 
+ */
 
 //dichiaro i primi elementi con i quali interagirò
 const objectContainer = document.querySelector('.container');
 const playButton = document.querySelector('button');
+
+let score = 0;
+const maxBombNumber = 16;
+let bombNumbersArray = [];
 
 //al click del play button viene generato il campo da gioco
 playButton.addEventListener('click', playFunction);
@@ -28,10 +49,38 @@ function playFunction() {
     generateNumeratedCard(i, objectContainer, gameDifficulty);
   };
 
+  bombNumbersArray = generateBombNumber(bombNumbersArray, maxBombNumber, playgroundDimension);
+
+  console.log(bombNumbersArray);
+
+  //const playgroundCard = document.getElementsByClassName('card');
+
+  return gameDifficulty;
+}
+
+
+function generateBombNumber(numericArray, maxArrayLenght, playgroundDimension) {
+  const min = 1;
+  const max = playgroundDimension;
+  let i = 0;
+  while(i < maxArrayLenght) {
+    let randomNumber = randomNumberGenerator(min, max);
+    if(!numericArray.includes(numericArray[i])) {
+      numericArray.push(randomNumber);
+      i++;
+    }
+  }
+  return numericArray;
+}
+
+function randomNumberGenerator(min, max){
+  const numberGenerated = Math.floor(Math.random() * (max - min + 1)) + min;
+  return numberGenerated;
 }
 
 function reset() {
   objectContainer.innerHTML = '';
+  bombNumbersArray = [];
 }
 
 /** la funzione crea la card e inserisce il numero
@@ -68,5 +117,24 @@ function generateCalcCss(numberOfElem){
  */
 function activateCard() {
   this.classList.add('active');
+  score++;
   console.log(this.idCard);
+
+  if(score === (gameDifficulty() - maxBombNumber)) {
+    winControl(true);
+  }
+
+  if(bombNumbersArray.includes(this.idCard)) {
+    winControl(false);
+  }
 };
+
+function endGame(winControl) {
+
+  if(winControl){
+    console.log("vinto");
+  }else{
+    console.log("perso con score: ", score);
+  }
+
+}
